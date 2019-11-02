181ddd58a8fde5522ec1da5d807423d46c04892d
Arnaud15 <arnaud.autef@gmail.com>
2019-11-02 10:39:32 -0700

    two more funcs in utils and adds

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import numpy as np
 from datetime import datetime, timedelta
+import os
+import glob
+
 
 def parse_edge_line(line):
     orig, dest = line.split("->")
@@ -10,6 +13,14 @@ def parse_edge_line(line):
     user_in, user_out = int(orig_list[1]), int(dest_list[1])
     time_in, time_out = float(orig_list[5]), float(dest_list[5])
     return tweet_in, tweet_out, user_in, user_out, time_in, time_out
+
+
+def get_root_id(tree_file_name):
+    return int(os.path.splitext(os.path.basename(tree_file_name))[0])
+
+
+def get_tree_file_names(datadir):
+    return glob.glob(os.path.join(datadir, "tree", "*.txt"))
 
 
 def one_hot_label(label):
